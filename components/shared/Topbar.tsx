62c207433d99664142185951658971eea9ce9327
thelordzeus <arnav55125@gmail.com>
2023-10-24 21:11:44 +0530

    feat: added assets file

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,17 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar() {
-  return <h1>TopBar</h1>;
+  return (
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.png" width={28} height={28} alt="logo"></Image>
+        <p className=" text-heading3-bold text-light-1 max-sm:hidden">
+          Threader
+        </p>
+      </Link>
+    </nav>
+  );
 }
 
 export default Topbar;
