d4ee60173e93b21e9599ffc48af4f9a5ac25d9c0
thelordzeus <arnav55125@gmail.com>
2023-10-24 20:02:40 +0530

    feat: added clerk auth

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import "../globals.css";
 
 export const metadata = {
   title: "Threader",
@@ -16,7 +17,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
     </ClerkProvider>
   );
