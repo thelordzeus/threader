34c649ccefe986eda772435a5d3682c7d9feafad
thelordzeus <arnav55125@gmail.com>
2023-10-25 20:40:08 +0530

    feat: added constants file

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 function LeftSidebar() {
-  return <section className=" custom-scrollbar leftsidebar"></section>;
+  return (
+    <section className=" custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6"></div>
+    </section>
+  );
 }
 
 export default LeftSidebar;
