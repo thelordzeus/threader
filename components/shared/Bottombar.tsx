2f020f4309864990b095d41c087d5040ce289c7c
thelordzeus <arnav55125@gmail.com>
2023-10-29 17:07:15 +0530

    feat: added bottom bar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,5 +1,40 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import Link from "next/link";
+
 function BottomBar() {
-  return <h1>BottomBar</h1>;
+  const router = useRouter();
+  const pathName = usePathname();
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathName.includes(link.route) && link.route.length > 1) ||
+            pathName === link.route;
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link
+            ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default BottomBar;
